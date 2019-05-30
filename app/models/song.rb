db6104ca6f7ffef5dccfd356e7d976914f277095
Savannah C <sgcorrell@gmail.com>
2019-05-30 20:52:47 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,6 +7,6 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    # when this method is called it should assign the song's artist to Drake
+    self.artist = Artist.find_or_create_by(name: "Drake")
   end
 end
